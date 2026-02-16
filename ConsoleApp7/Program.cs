a9963a69279191254e5ac8bce09c816a4cb6876c
molnarda <molnarda@310-04>
2026-02-16 09:29:14 +0100

    1.

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ConsoleApp7
+﻿using System.Text.Json.Nodes;
+
+namespace ConsoleApp7
 {
+    class adat
+    { 
+     public List<string> nevek { get; set; }
+
+     public List<int> korok { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string fajl = File.ReadAllText("json.json",System.Text.Encoding.Latin1);
+            Console.WriteLine(fajl);
         }
     }
 }
