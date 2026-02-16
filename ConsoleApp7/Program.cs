c2c6938636db42bb2e492d3dc4f1f79cfa08b9aa
molnarda <molnarda@310-04>
2026-02-16 09:36:11 +0100

    adatok.json feldolgozasa

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,19 +1,27 @@
-﻿using System.Text.Json.Nodes;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace ConsoleApp7
 {
     class adat
-    { 
-     public List<string> nevek { get; set; }
+    {
+        public List<string> nevek { get; set; }
 
-     public List<int> korok { get; set; }
+        public List<int> korok { get; set; }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            string fajl = File.ReadAllText("json.json",System.Text.Encoding.Latin1);
+            string fajl = File.ReadAllText("json.json", System.Text.Encoding.Latin1);
             Console.WriteLine(fajl);
+            adat adat = JsonSerializer.Deserialize<adat>(fajl);
+
+            foreach (var nev in adat.nevek)
+            {
+                Console.WriteLine(nev);
+            }
+            Console.WriteLine($"{adat.nevek[0]} életkora: { adat.korok[0]}");
         }
     }
 }
